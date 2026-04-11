75eb26025e9173254e64f1615b1d4d0d94262c7d
Mike Naberezny <mike@naberezny.com>
2026-04-11 09:37:28 -0700

    Remove phpBB legalese from forum registration

--- language/en/ucp.php
+++ language/en/ucp.php
@@ -38,12 +38,7 @@ if (empty($lang) || !is_array($lang))
 
 // Privacy policy and T&C
 $lang = array_merge($lang, array(
-	'TERMS_OF_USE_CONTENT'	=> 'By accessing “%1$s” (hereinafter “we”, “us”, “our”, “%1$s”, “%2$s”), you agree to be legally bound by the following terms. If you do not agree to be legally bound by all of the following terms then please do not access and/or use “%1$s”. We may change these at any time and we’ll do our utmost in informing you, though it would be prudent to review this regularly yourself as your continued usage of “%1$s” after changes mean you agree to be legally bound by these terms as they are updated and/or amended.
-	<br><br>
-	Our forums are powered by phpBB (hereinafter “they”, “them”, “their”, “phpBB software”, “www.phpbb.com”, “phpBB Limited”, “phpBB Teams”) which is a bulletin board solution released under the “<a href="http://opensource.org/licenses/gpl-2.0.php">GNU General Public License v2</a>” (hereinafter “GPL”) and can be downloaded from <a href="https://www.phpbb.com/">www.phpbb.com</a>. The phpBB software only facilitates internet based discussions; phpBB Limited is not responsible for what we allow and/or disallow as permissible content and/or conduct. For further information about phpBB, please see: <a href="https://www.phpbb.com/">https://www.phpbb.com/</a>.
-	<br><br>
-	You agree not to post any abusive, obscene, vulgar, slanderous, hateful, threatening, sexually-orientated or any other material that may violate any laws be it of your country, the country where “%1$s” is hosted or International Law. Doing so may lead to you being immediately and permanently banned, with notification of your Internet Service Provider if deemed required by us. The IP address of all posts are recorded to aid in enforcing these conditions. You agree that “%1$s” have the right to remove, edit, move or close any topic at any time should we see fit. As a user you agree to any information you have entered to being stored in a database. While this information will not be disclosed to any third party without your consent, neither “%1$s” nor phpBB shall be held responsible for any hacking attempt that may lead to the data being compromised.
-	',
+		'TERMS_OF_USE_CONTENT'	=> '<p>By registering for an account on the forum, you agree to:</p><ul><li><strong>Maintain focus.</strong> Keep posts focused on the 6502 and its relatives, electronics, computers, and the like. Please leave politics, religion, and other unrelated topics at the door.</li><li><strong>Be respectful.</strong> Be kind and helpful to other members. Personal attacks, insults, and condescending behavior have no place here. Remember that our members range from absolute beginners to industry veterans.</li><li><strong>Keep it family-friendly.</strong> People of all ages use this forum, so please do not post vulgar language or inappropriate content.</li><li><strong>Preserve your posts.</strong> Don\'t request your posts be deleted because it disrupts the thread history for everyone else.</li><li><strong>Respect our process.</strong> Leave moderating to the moderators. Do not engage with questionable content. Use the "report post" button instead. Moderators have the final word on what is acceptable.</li></ul><p>We reserve the right to remove content or ban accounts at our discretion. Be cool to others, talk about your projects, and have a good time.</p>',
 
 	'PRIVACY_POLICY'		=> 'This policy explains in detail how “%1$s” along with its affiliated companies (hereinafter “we”, “us”, “our”, “%1$s”, “%2$s”) and phpBB (hereinafter “they”, “them”, “their”, “phpBB software”, “www.phpbb.com”, “phpBB Limited”, “phpBB Teams”) use any information collected during any session of usage by you (hereinafter “your information”).
 	<br><br>
@@ -82,7 +77,7 @@ $lang = array_merge($lang, array(
 	'ADD_TO'						=> 'Add [To]',
 	'ADD_USERS_UCP_EXPLAIN'			=> 'Here you can add new users to the group. You may select whether this group becomes the new default for the selected users. Please enter each username on a separate line.',
 	'ADMIN_EMAIL'					=> 'Administrators can email me information',
-	'AGREE'							=> 'I agree to these terms',
+	'AGREE'							=> 'I\'ll play well with others',
 	'ALLOW_PM'						=> 'Allow users to send you private messages',
 	'ALLOW_PM_EXPLAIN'				=> 'Note that administrators and moderators will always be able to send you messages.',
 	'ALREADY_ACTIVATED'				=> 'You have already activated your account.',
@@ -364,7 +359,7 @@ $lang = array_merge($lang, array(
 	'NOT_ADDED_FOES_BOTS'			=> 'You cannot block bots.',
 	'NOT_ADDED_FOES_FRIENDS'		=> 'You cannot block users who are on your bookmarked list.',
 	'NOT_ADDED_FOES_SELF'			=> 'You cannot block yourself.',
-	'NOT_AGREE'						=> 'I do not agree to these terms',
+	'NOT_AGREE'						=> 'This isn\'t the place for me',
 	'NOT_ENOUGH_SPACE_FOLDER'		=> 'The destination folder “%s” seems to be full. The requested action has not been taken.',
 	'NOT_MOVED_MESSAGES'			=> array(
 		1	=> 'You have %d private message currently on hold because of full folder.',
